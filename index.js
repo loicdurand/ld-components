3916f07f90a2c9c0ce2e29c3ae3891bca19bb240
Loïc DURAND <loicdurand.ld@gmail.com>
2018-06-21 14:48:04 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import { h } from 'hyperapp'
+import picostyle from "picostyle"
+const style = picostyle(h);
+
+export class Komponent {
+    constructor(o) {
+        o.style = o.style || function () {
+            return {};
+        };
+        if (o)
+            return style(o.jsx)(o.style);
+        else
+            return this;
+    }
+}
