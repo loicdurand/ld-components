195ff2d5d8c4804232d9d17f7a4d51357b9950f6
Loïc DURAND <loicdurand.ld@gmail.com>
2018-11-22 15:47:07 +0100

    transform es5 to es6; copied picostyle's original "keyframes" function to my script

--- index.js
+++ index.js
@@ -1,94 +1,90 @@
 import { h, app } from 'hyperapp'
 
-var _id = 0
-var sheet = document.head.appendChild(document.createElement("style")).sheet
-
-function hyphenate(str) {
-    return str.replace(/[A-Z]/g, "-$&").toLowerCase()
-}
-
-function insert(rule) {
-    sheet.insertRule(rule, sheet.cssRules.length)
-}
-
-function createStyle(obj) {
-    var id = "P" + _id++
-    if (Array.isArray(obj)) {
-        let // 
-            css = obj[0],
-            wrapper = "." + id,
-            selectors = /[^;]*{/g;
-        if (!selectors.test(css))
-            insert(wrap(obj, wrapper));
-        else {
-            let // 
-                wrap = wrapper + "{" + (css
-                    .replace(selectors, m => '}' + wrapper + m)
-                    .replace(/&/g, wrapper)
-                ),
-                rules = wrap.split(/}/);
-            rules.pop();
-            rules.map(rule => insert(rule + '}'))
-        }
-    } else {
-        parse(obj, "." + id).forEach(insert)
-    }
-    return id
-}
-function wrap(stringToWrap, wrapper) {
-    return wrapper + "{" + stringToWrap + "}"
-}
-
-function parse(obj, classname, isInsideObj) {
-    var arr = [""]
-    isInsideObj = isInsideObj || 0
-    for (var prop in obj) {
-        var value = obj[prop]
-        prop = hyphenate(prop)
-        // Same as typeof value === 'object', but smaller
-        if (!value.sub && !Array.isArray(value)) {
-            if (/^(:|>|\.|\*)/.test(prop)) {
-                prop = classname + prop
+let _id = 0;
+const //
+    sheet = document.head.appendChild(document.createElement("style")).sheet,
+    wrap = (stringToWrap, wrapper) => wrapper + "{" + stringToWrap + "}",
+    hyphenate = str => str.replace(/[A-Z]/g, "-$&").toLowerCase(),
+    insert = rule => sheet.insertRule(rule, sheet.cssRules.length),
+    parse = (obj, classname, isInsideObj) => {
+        var arr = [""]
+        isInsideObj = isInsideObj || 0
+        for (var prop in obj) {
+            var value = obj[prop]
+            prop = hyphenate(prop)
+            // Same as typeof value === 'object', but smaller
+            if (!value.sub && !Array.isArray(value)) {
+                if (/^(:|>|\.|\*)/.test(prop)) {
+                    prop = classname + prop
+                }
+                // replace & in "&:hover", "p>&"
+                prop = prop.replace(/&/g, classname)
+                arr.push(
+                    wrap(parse(value, classname, 1 && !/^@/.test(prop)).join(""), prop)
+                )
+            } else {
+                value = Array.isArray(value) ? value : [value]
+                value.forEach((value) => {
+                    arr[0] += prop + ":" + value + ";"
+                })
             }
-            // replace & in "&:hover", "p>&"
-            prop = prop.replace(/&/g, classname)
-            arr.push(
-                wrap(parse(value, classname, 1 && !/^@/.test(prop)).join(""), prop)
-            )
+        }
+        if (!isInsideObj) {
+            arr[0] = wrap(arr[0], classname)
+        }
+        return arr
+    },
+    createStyle = obj => {
+        var id = "P" + _id++
+        if (Array.isArray(obj)) {
+            let // 
+                css = obj[0],
+                wrapper = "." + id,
+                selectors = /[^;]*{/g;
+            if (!selectors.test(css))
+                insert(wrap(obj, wrapper));
+            else {
+                let // 
+                    wrap = wrapper + "{" + (css
+                        .replace(selectors, m => '}' + wrapper + m)
+                        .replace(/&/g, wrapper)
+                    ),
+                    rules = wrap.split(/}/);
+                rules.pop();
+                rules.map(rule => insert(rule + '}'))
+            }
         } else {
-            value = Array.isArray(value) ? value : [value]
-            value.forEach((value) => {
-                arr[0] += prop + ":" + value + ";"
-            })
+            parse(obj, "." + id).forEach(insert)
         }
-    }
-    if (!isInsideObj) {
-        arr[0] = wrap(arr[0], classname)
-    }
-    return arr
-}
-
-const picostyle = function (nodeName) {
-    var cache = {}
-    return function (decls) {
-        decls = typeof decls == 'string' ? [decls] : decls
-        return function (attributes, children) {
-            attributes = attributes || {}
-            children = attributes.children || children
-            var nodeDecls = typeof decls == "function" ? decls(attributes) : decls
-            var key = JSON.stringify(nodeDecls)
-            cache[key] || (cache[key] = createStyle(nodeDecls))
-            attributes.class = [attributes.class, cache[key]]
-                .filter(Boolean)
-                .join(" ")
-            return h(nodeName, attributes, children)
+        return id
+    },
+    picostyle = nodeName => {
+        var cache = {}
+        return (decls) => {
+            decls = typeof decls == 'string' ? [decls] : decls
+            return (attributes, children) => {
+                attributes = attributes || {}
+                children = attributes.children || children
+                var nodeDecls = typeof decls == "function" ? decls(attributes) : decls
+                var key = JSON.stringify(nodeDecls)
+                cache[key] || (cache[key] = createStyle(nodeDecls))
+                attributes.class = [attributes.class, cache[key]]
+                    .filter(Boolean)
+                    .join(" ")
+                return h(nodeName, attributes, children)
+            }
         }
-    }
-}
+    };
 
 export default new Proxy(picostyle, {
     get: (target, key) => target(key)
 });
+
+export function keyframes(obj) {
+    var id = "p" + _id++
+    insert(wrap(parse(obj, id, 1).join(""), "@keyframes " + id))
+    return id
+}
 
 export { h, app };
 
