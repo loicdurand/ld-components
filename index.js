988ca134f04dbb7ae9c23bb380392283a7c6a0ff
Loïc DURAND <loicdurand.ld@gmail.com>
2018-11-23 17:09:10 +0100

    imprtant code refactoring: removed "keyframes" function that I don't need, and css-in-json notation (replaced by template strings )

--- index.js
+++ index.js
@@ -1,113 +1,46 @@
-import {
-    h,
-    app
-} from 'hyperapp'
+import { h, app } from 'hyperapp';
 
 let _id = 0;
+
 const //
+    cache = {},
     sheet = document.head.appendChild(document.createElement("style")).sheet,
-    wrap = (stringToWrap, wrapper) => wrapper + "{" + stringToWrap + "}",
-    hyphenate = str => str.replace(/[A-Z]/g, "-$&").toLowerCase(),
     insert = rule => sheet.insertRule(rule, sheet.cssRules.length),
-    parse = (obj, classname, isInsideObj) => {
-        var arr = [""]
-        isInsideObj = isInsideObj || 0
-        for (var prop in obj) {
-            var value = obj[prop]
-            prop = hyphenate(prop)
-                // Same as typeof value === 'object', but smaller
-            if (!value.sub && !Array.isArray(value)) {
-                if (/^(:|>|\.|\*)/.test(prop)) {
-                    prop = classname + prop
-                }
-                // replace & in "&:hover", "p>&"
-                prop = prop.replace(/&/g, classname)
-                arr.push(
-                    wrap(parse(value, classname, 1 && !/^@/.test(prop)).join(""), prop)
-                )
-            } else {
-                value = Array.isArray(value) ? value : [value]
-                value.forEach((value) => {
-                    arr[0] += prop + ":" + value + ";"
-                })
+    isProp = str => /.*:.*;/.test(str),
+    isSelector = str => /^(:|>|\.|\*)/.test(str),
+    isClosingBracket = str => /\s?}\s?/.test(str),
+    isMediaQuery = str => /^@/.test(str),
+    createStyle = decls => {
+        let // 
+            id = ".P" + _id++,
+            i = 0,
+            rule = '',
+            endline = /;|}|{/g,
+            lines = decls.replace(/\s+/g, ' ').replace(endline, m => m + '|').split('|');
+        lines.map(line => {
+            line = line.trim();
+            if (isProp(line)) {
+                rule += (i++ == 0 ? id + '{' + line : line);
+            } else if (!isClosingBracket(line)) {
+                insert(rule + '}');
+                rule = '';
+                rule += isSelector(line) ? (id + line.replace(/&/g, id)) :
+                    (i = +isMediaQuery(line) ? 0 : i) ? (rule += line) : '';
             }
-        }
-        if (!isInsideObj) {
-            arr[0] = wrap(arr[0], classname)
-        }
-        return arr
+        });
+        return id.slice(1);
     },
-    createStyle = obj => {
-        var id = "P" + _id++
-            if (Array.isArray(obj)) {
-                let // 
-                    css = obj[0],
-                    wrapper = "." + id,
-                    selectors = /[^;]*{/g;
-                if (!selectors.test(css))
-                    insert(wrap(obj, wrapper));
-                else {
-                    let // 
-                        arr = [],
-                        rules = [],
-                        i = 0,
-                        endline = /;|}|{/g,
-                        lines = css.replace(/\s+/g, ' ').replace(endline, m => m + '|').split('|'),
-                        isProp = str => /.*:.*;/.test(str),
-                        isSelector = str => /^(:|>|\.|\*)/.test(str),
-                        isClosingBracket = str => str == '}',
-                        isMediaQuery = str => /^@/.test(str);
-                    lines.map(line => {
-                        line = line.trim();
-                        if (isProp(line)) {
-                            arr.push(i++ == 0 ? wrapper + '{' + line : line);
-                        } else if (isSelector(line)) {
-                            arr.push('}' + wrapper + line.replace(/&/g, wrapper));
-                        } else if (isClosingBracket(line)) {
-                            i = 0;
-                            arr.push(line);
-                        } else if (isMediaQuery(line)) {
-                            arr.push(line);
-                        }
-                    });
-                    rules = arr.join('').split('}');
-                    rules.pop();
-                    rules.map(rule => insert(rule + '}'));
-                }
-            } else {
-                parse(obj, "." + id).forEach(insert)
-            }
-        return id
-    },
-    picostyle = nodeName => {
-        var cache = {}
-        return (decls) => {
-            decls = typeof decls == 'string' ? [decls] : decls
-            return (attributes, children) => {
-                attributes = attributes || {}
-                children = attributes.children || children
-                var nodeDecls = typeof decls == "function" ? decls(attributes) : decls
-                var key = JSON.stringify(nodeDecls)
-                cache[key] || (cache[key] = createStyle(nodeDecls))
-                attributes.class = [attributes.class, cache[key]]
-                    .filter(Boolean)
-                    .join(" ")
-                return h(nodeName, attributes, children)
-            }
-        }
+    picostyle = nodeName => decls => (attributes = {}, children = attributes.children) => {
+        let key = typeof decls == "function" ? decls(attributes) : Array.isArray(decls) ? decls[0] : decls;
+        cache[key] || (cache[key] = createStyle(key));
+        attributes.class = [attributes.class, cache[key]]
+            .filter(Boolean)
+            .join(" ");
+        return h(nodeName, attributes, children);
     };
 
 export default new Proxy(picostyle, {
     get: (target, key) => target(key)
 });
 
-export function keyframes(obj) {
-    var id = "p" + _id++
-        insert(wrap(parse(obj, id, 1).join(""), "@keyframes " + id))
-    return id
-}
-
-export {
-    h,
-    app
-};
+export { h, app };
--- test/index.js
+++ test/index.js
@@ -1,26 +1,112 @@
 import style, { h, app } from 'ld-components';
 
+// export const Col = (props, children)=>{
+//     console.log(props)
+// }
+
+const Row = style.div`
+    background: rgba(0,0,0,.2);
+    margin-left: -15px;
+    margin-right: -15px;
+    :before, &:after {
+        display: table;
+        content: "";
+        clear: both;
+    }`;
+
+
+
+const Col = style.div(
+    props => {
+        props.s = props.s || 12;
+        props.m = props.m || props.s;
+        props.l = props.l || props.m;
+        props.offsetS = props.offsetS || 0;
+        props.offsetM = props.offsetM || props.offsetS;
+        props.offsetL = props.offsetL || props.offsetM;
+        // return {
+        //     paddingLeft: '15px',
+        //     paddingRight: '15px',
+        //     float: 'left',
+        //     position: 'relative',
+        //     "@media all and (min-width: 0px) and (max-width: 639px)": {
+        //         width: `${100 / 12 * props.s}%`,
+        //         marginLeft: `${100 / 12 * props.offsetS}%`
+        //     },
+        //     "@media all and (min-width: 640px) and (max-width: 991px)": {
+        //         width: `${100 / 12 * props.m}%`,
+        //         marginLeft: `${100 / 12 * props.offsetM}%`
+        //     },
+        //     "@media all and (min-width: 992px)": {
+        //         width: `${100 / 12 * props.l}%`,
+        //         marginLeft: `${100 / 12 * props.offsetL}%`
+        //     }
+        // }
+        return `
+            display: block;
+            background: rgba(0,0,0,.4);
+            padding-left: 15px;
+            padding-right: 15px;
+            float: left;
+            position: relative;
+            @media all and (min-width: 0px) and (max-width: 639px) {
+                width: ${100 / 12 * props.s}%;
+                margin-left: ${100 / 12 * props.offsetS}%;
+            }
+            @media all and (min-width: 640px) and (max-width: 991px) {
+                width: ${100 / 12 * props.m}%;
+                margin-left: ${100 / 12 * props.offsetM}%;
+            }
+            @media all and (min-width: 992px) {
+                width: ${100 / 12 * props.m}%;
+                margin-left: ${100 / 12 * props.offsetL}%;
+            }
+        `
+    }
+)
+
+const Span = style('span')(`
+    color: blue;
+    background: red;
+    text-transform: uppercase;
+    .bold {
+        font-weight: bold;
+    }
+`)
+
+
 const // 
-state = {},
-actions = {},
-Test = style.div`
+    state = {},
+    actions = {},
+    Test = style.div`
     color: red;
     text-transform: uppercase;
     :before, &:after {
         content: "z";
         color: blue;
-        font-size: 12px;
+        font-size: 4em;
     }
     @media all and (max-width: 600px) {
-        font-size: 32px;
+        text-decoration: underline;
         font-style: italic;
     }
 `,
 
-view = (state, actions)=>(
-    <Test>hello
-        <span>world</span>
-    </Test>
-);
+    view = (state, actions) => (
+        <Row>
+             <Col s="3" m="4" l="6">
+                 <Span>hello</Span>
+             </Col>
+            <Col s="3" m="4" l="4">
+                <Span class="bold">world</Span>
+             </Col>
+            <Col s="3" m="4" l="3">
+                 <Span>!!!</Span>
+                 <Test/>
+             </Col>
+
+        </Row>
+
+    );
 
 app(state, actions, view, document.body);
